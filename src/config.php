beca2c3db49ad2c897736be5b38a78ed7d1093de
AdrianPalacios04 <adrian.palacios@tecsup.edu.pe>
2020-07-07 23:01:09 -0500

    first commit

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => 'us-cdbr-east-02.cleardb.com',
+    'username' => 'ba460c695ff89b',
+    'password' => '2c7e6dae',
+    'db' => 'heroku_e26d04748737492' //Cambiar al nombre de tu base de datos
+];
+
+?>
